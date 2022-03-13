ae4055d2a167fa3a925058db8a033077ef3a5823
Wojsznis <wojsznis1997@gmail.com>
2022-03-14 00:29:06 +0100

    Core Game

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [Header("Shooting")]
+    [SerializeField] AudioClip shootingClip;
+    [SerializeField] [Range(0f, 1f)] float shootingVolume;
+
+    [Header("Damage")]
+    [SerializeField] AudioClip damageClip;
+    [SerializeField] [Range(0f, 1f)] float damageVolume;
+
+    static AudioPlayer instance;
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        //int instanceCount = FindObjectsOfType(GetType()).Length;
+        //if(instanceCount > 1)
+        if(instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void PlayShootingClip()
+    {
+        if(shootingClip != null)
+        {
+            PlaClip(shootingClip, shootingVolume);
+        }
+    }
+
+    public void PlayDamageClip()
+    {
+        PlaClip(damageClip, damageVolume);
+    }
+
+    void PlaClip(AudioClip clip, float volume)
+    {
+        if(clip != null)
+        {
+            Vector3 cameraPos = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(clip, cameraPos, volume);
+        }
+    }
+}
