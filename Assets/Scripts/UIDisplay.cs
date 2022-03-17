a77fb5f9370c9784cd0a957e51226ff8f76e562b
Paweł Wojszni <63476210+Wojsznis@users.noreply.github.com>
2022-03-17 01:00:38 +0100

    Revert "Added Heal power UP"

--- Assets/Scripts/UIDisplay.cs
+++ Assets/Scripts/UIDisplay.cs
@@ -13,7 +13,7 @@ public class UIDisplay : MonoBehaviour
     [Header("Score")]
     [SerializeField] TextMeshProUGUI scoreText;
     ScoreKeeper scoreKeeper;
-    
+
     void Awake()
     {
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
